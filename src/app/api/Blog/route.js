e96be4519a4f3e771185e02fc7be2e5cb0d3ca26
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-07-10 11:36:56 +0600

    router.js setup basic

---
+++ src/app/api/Blog/route.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export const GET =()=>{
+    return NextResponse.json({message:'Hellow world'});
+
+};
+
+export const POST =()=>{
+    return NextResponse.json({message:'Hellow world'});
+
+};
+
+export const PATCH =()=>{
+    return NextResponse.json({message:'Hellow world'});
+
+};
+
+export const DELETE =()=>{
+    return NextResponse.json({message:'Hellow world'});
+
+};
--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+import styles from'./Posts.module.css'
 
 const PostsPage = async() => {
     const res=await fetch("http://localhost:5000/posts",{
@@ -15,7 +16,7 @@ const PostsPage = async() => {
 //    console.log(posts);
     return (
         <div className="w-full   ">
-            <h1 className="text-xl">Total data posts:{posts.length}</h1>
+            <h1 className={styles.header_text} >Total data posts:{posts.length}</h1>
 
             { posts.map(post=> 
             <div key={post.id} className="card bg-primary text-primary-content w-[70%] my-4 mx-auto">
